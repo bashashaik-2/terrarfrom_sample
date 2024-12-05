986c10536a53fe7643ba76d8991056da16c53ec4
bashashaik-2 <sameerbasha2207@gmail.com>
2024-12-05 17:23:31 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,15 @@
+variable "filename" {
+default ="pets.txt"
+}
+variable "content" {
+default ="we love animals!"
+}
+variable "prefix" {
+default ="MISS"
+}
+variable "separator" {
+default =","
+}
+variable "length" {
+default ="1"
+}
